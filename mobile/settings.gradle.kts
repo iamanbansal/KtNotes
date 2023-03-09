82aaee0b4d56659c5a063c1cec987d6a93cf2c96
Aman Bansal <aman210697@gmail.com>
2023-03-09 21:10:37 +0530

    setup kmm project

---
+++ mobile/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KtNotes"
+include(":androidApp")
+include(":shared")
