20e490d57e9b0ddfb3737e59053b99382b573219
Aman Bansal <aman210697@gmail.com>
2023-03-15 19:33:58 +0530

    set base url

--- mobile/shared/src/commonMain/kotlin/com/ktnotes/di/networkModule.kt
+++ mobile/shared/src/commonMain/kotlin/com/ktnotes/di/networkModule.kt
@@ -1,39 +1,44 @@
 package com.ktnotes.di
 
+import com.ktnotes.feature.auth.remote.AuthApi
+import com.ktnotes.feature.auth.remote.AuthApiImpl
+import com.ktnotes.feature.auth.remote.AuthRepository
+import com.ktnotes.feature.auth.remote.AuthRepositoryImpl
+import com.ktnotes.session.SessionManager
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.plugins.HttpSend
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.plugin
-import io.ktor.client.request.headers
-import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 fun networkModule(enableNetworkLogs: Boolean) = module {
     single { createJson() }
-    single { createHttpClient(get(), get(), enableNetworkLogs = enableNetworkLogs) }
+    single { createHttpClient(get(), get(), get(), enableNetworkLogs = enableNetworkLogs) }
 }
 
-fun createJson() = Json { isLenient = true; ignoreUnknownKeys = true }
+fun createJson() = Json {
+    isLenient = true; ignoreUnknownKeys = true;
+}
 
-fun createHttpClient(httpClientEngine: HttpClientEngine, json: Json, enableNetworkLogs: Boolean) =
-    HttpClient(httpClientEngine) {
+fun createHttpClient(
+    httpClientEngine: HttpClientEngine,
+    json: Json,
+    sessionManager: SessionManager,
+    enableNetworkLogs: Boolean
+): HttpClient {
+    val client = HttpClient(httpClientEngine) {
         install(ContentNegotiation) {
             json(json)
         }
 
         defaultRequest {
-            host = "my.zoo.com"
-            url {
-                protocol = URLProtocol.HTTPS
-            }
+            url(BASE_URL)
         }
         if (enableNetworkLogs) {
             install(Logging) {
@@ -42,3 +47,10 @@ fun createHttpClient(httpClientEngine: HttpClientEngine, json: Json, enableNetwo
             }
         }
     }
+
+
+    return client
+}
+
+const val BASE_URL = "https://ktnotes-production.up.railway.app"
+const val AUTH_HEADER_KEY = "Authorization"
