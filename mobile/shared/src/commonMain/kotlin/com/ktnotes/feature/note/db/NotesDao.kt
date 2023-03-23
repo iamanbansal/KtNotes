60d87889726f93d2a3327cc8bc16f2e9e3ac616c
Aman Bansal <aman210697@gmail.com>
2023-03-24 01:56:50 +0530

    ⬆️ bulk inserts and update note

--- mobile/shared/src/commonMain/kotlin/com/ktnotes/feature/note/db/NotesDao.kt
+++ mobile/shared/src/commonMain/kotlin/com/ktnotes/feature/note/db/NotesDao.kt
@@ -3,16 +3,18 @@ package com.ktnotes.feature.note.db
 import com.ktnotes.database.KtNotesDatabase
 import com.ktnotes.feature.note.mapper
 import com.ktnotes.feature.note.model.Note
-import com.ktnotes.feature.note.toNote
 import com.squareup.sqldelight.runtime.coroutines.asFlow
 import com.squareup.sqldelight.runtime.coroutines.mapToList
 import kotlinx.coroutines.flow.Flow
 
 interface NotesDao {
     suspend fun insertNote(note: Note)
+    suspend fun insertNotes(notes: List<Note>)
     suspend fun getNoteById(id: String): Note?
     suspend fun getAllNotes(): Flow<List<Note>>
     suspend fun deleteNoteById(id: String)
+    suspend fun updateNote(note: Note)
+    suspend fun clearAll()
 }
 
 
@@ -20,17 +22,28 @@ class NotesDaoImpl(notesDatabase: KtNotesDatabase) : NotesDao {
     private val queries = notesDatabase.noteQueries
 
     override suspend fun insertNote(note: Note) {
-        queries.inserNote(
+        insert(note)
+    }
+
+    override suspend fun insertNotes(notes: List<Note>) {
+        queries.transaction {
+            //todo use afterRollback{} in case error
+            notes.forEach { insert(it) }
+        }
+    }
+
+    private fun insert(note: Note) {
+        queries.inserOrReplaceNote(
             id = note.id,
             title = note.title,
             note = note.note,
-            createdAt = note.createdAt,
-            updatedAt = note.updatedAt,
+            createdAt = note.created,
+            updatedAt = note.updated,
             isPinned = note.isPinned
         )
     }
 
-    override suspend fun getNoteById(id: String,): Note? {
+    override suspend fun getNoteById(id: String): Note? {
         return queries.getNoteById(id, mapper).executeAsOneOrNull()
     }
 
@@ -40,6 +53,21 @@ class NotesDaoImpl(notesDatabase: KtNotesDatabase) : NotesDao {
 
     override suspend fun deleteNoteById(id: String) {
         queries.deleteNoteById(id)
+    }
+
+    override suspend fun updateNote(note: Note) {
+        queries.inserOrReplaceNote(
+            id = note.id,
+            title = note.title,
+            note = note.note,
+            createdAt = note.created,
+            updatedAt = note.updated,
+            isPinned = note.isPinned
+        )
+    }
+
+    override suspend fun clearAll() {
+        queries.clearAllNotes()
     }
 }
 
