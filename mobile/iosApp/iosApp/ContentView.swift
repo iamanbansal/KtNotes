07cd110b962505076f3a2fa5628c05407b324be1
Aman Bansal <aman210697@gmail.com>
2023-03-31 18:00:37 +0530

    🔐 ios RegistrationView

--- mobile/iosApp/iosApp/ContentView.swift
+++ mobile/iosApp/iosApp/ContentView.swift
@@ -1,17 +1,25 @@
 import SwiftUI
 import shared
+import Combine
+
 
 struct ContentView: View {
-	let greet = Greeting().greet()
-
-	var body: some View {
-		Text(greet)
-	}
+    @State private var isLoggedIn = true
+       var body: some View {
+           NavigationView {
+               VStack {
+                   if isLoggedIn {
+                       NoteListScreen(onLogout: {isLoggedIn = false})
+                   }
+                   else {
+                       LoginView(onLogin: {isLoggedIn = true})
+                   }
+                   
+               }
+               .transition(.opacity)
+               .animation(.easeInOut)
+           }
+       }
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-        
-	}
-}
+
---
+++ mobile/iosApp/iosApp/RegistrationView.swift
@@ -0,0 +1,127 @@
+//
+//  RegistrationView.swift
+//  iosApp
+//
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import shared
+import Combine
+
+struct LoginView: View {
+    var onLogin:()->Void
+    
+    @StateObject var viewModel = ObservableAuthModel()
+    @State private var name = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var isLogin = false
+    @State private var title = "Signup"
+    
+    var body: some View {
+            VStack {
+                Spacer()
+                if !isLogin {
+                    TextField("Name", text: $name)
+                }
+                
+                TextField("Email", text: $email)
+                SecureField("Password", text: $password)
+
+                if isLogin {
+                    Button("Login") {
+                        viewModel.login(email: email, password: password)
+                    }
+                } else {
+                    Button("Signup") {
+                        viewModel.register(name: name, email: email, password: password)
+                    }
+                }
+                Spacer()
+
+                if isLogin {
+                    HStack {
+                        Text("Don't have an account?")
+                        Button("Signup") {
+                            isLogin = false
+                        }.foregroundColor(.blue)
+                    }
+                } else {
+                    HStack {
+                        Text("Already have an account?")
+                        Button("Login") {
+                            isLogin = true
+                        }.foregroundColor(.blue)
+                    }
+                }
+
+            }.padding()
+            .onAppear(perform: {
+                self.viewModel.activate()
+                self.title = isLogin ? "Login" : "Signup"
+            })
+            .onDisappear(perform: {
+                self.viewModel.deactivate()
+            })
+            .onChange(of: viewModel.uiState?.isLoggedIn, perform: { isLoggedIn in
+                       if isLoggedIn == true {
+                           onLogin()
+                       }
+                   })
+        
+            .onChange(of: isLogin, perform: { isLogin in
+                self.title = isLogin ? "Login" : "Signup"
+                })
+        
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitle(title)
+        
+        }
+
+}
+
+class ObservableAuthModel: ObservableObject {
+    
+    private var viewmodel: AuthCallbackViewModel?
+    private var cancellables = [AnyCancellable]()
+    
+    @Published var error: String?
+    @Published var uiState:LoginUiState?
+    
+    func activate(){
+        let viewmodel  =  KotlinDependencies.shared.getCallbackAuthViewModel()
+        
+        doPublish(viewmodel.stateAdapter){[weak self] authState in
+            self?.error = authState.message
+            self?.uiState = authState
+        }
+        .store(in: &cancellables)
+        
+        
+        self.viewmodel = viewmodel
+    }
+    func login(email: String, password: String) {
+        // Login logic
+        viewmodel?.login(email: email, password: password)
+    }
+
+    func register(name: String, email: String, password: String) {
+        viewmodel?.register(name: name, email: email, password: password)
+    }
+    
+    func deactivate(){
+        cancellables.forEach { $0.cancel() }
+        cancellables.removeAll()
+
+        viewmodel?.clear()
+        viewmodel = nil
+}
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView(onLogin: {})
+    }
+}
