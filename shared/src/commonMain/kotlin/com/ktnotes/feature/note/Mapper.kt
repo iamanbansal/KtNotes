04eaf6930e70375537a9f448fc4a979ccaab25de
Aman Bansal <aman210697@gmail.com>
2023-04-30 18:21:04 +0530

    unsynced notes with operation

--- shared/src/commonMain/kotlin/com/ktnotes/feature/note/Mapper.kt
+++ shared/src/commonMain/kotlin/com/ktnotes/feature/note/Mapper.kt
@@ -2,6 +2,7 @@ package com.ktnotes.feature.note
 
 import com.ktnotes.database.NoteEntity
 import com.ktnotes.feature.note.model.Note
+import com.ktnotes.feature.note.model.NoteWithOperation
 
 fun NoteEntity.toNote(): Note {
     return Note(id, title, note, createdAt, updatedAt, isPinned)
@@ -18,3 +19,14 @@ val mapper: (
     Note(id, title, note, createdAt, updatedAt, isPinned)
 }
 
+val noteOpMapper: (
+    id: String,
+    title: String,
+    note: String,
+    createdAt: Long,
+    updatedAt: Long,
+    isPinned: Boolean,
+    operation:Long
+) -> NoteWithOperation = { id, title, note, createdAt, updatedAt, isPinned, operation ->
+    NoteWithOperation(Note(id, title, note, createdAt, updatedAt, isPinned), operation.toInt())
+}
